b56ad474f204e98cf54a65b97d59b25ca166440e
Byreddy Pranitha <byreddypranitha@gmail.com>
2026-03-15 16:38:58 +0530

    Trying Live Navigation

--- campus-navigator/frontend/src/navigation/AppNavigator.js
+++ campus-navigator/frontend/src/navigation/AppNavigator.js
@@ -4,10 +4,6 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import HomeScreen from '../screens/HomeScreen';
 import NavigationHomeScreen from '../screens/navigation/NavigationHomeScreen';
-<<<<<<< HEAD
-
-=======
->>>>>>> 6391fe06a9906ac2a0d7ff969ca28e6be1a3ccb7
 // 1. IMPORT your detail screen here (verify the exact file path)
 import LocationDetailScreen from '../screens/navigation/LocationDetailScreen'; 
 
@@ -28,10 +24,6 @@ export default function AppNavigator() {
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="NavigationHome" component={NavigationHomeScreen} />
-<<<<<<< HEAD
-        
-=======
->>>>>>> 6391fe06a9906ac2a0d7ff969ca28e6be1a3ccb7
         
         {/* 2. REGISTER the screen with the name 'LocationDetail' */}
         <Stack.Screen name="LocationDetail" component={LocationDetailScreen} />
